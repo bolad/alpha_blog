4826b880bb6a0b5ece298c477ba3d1886c50b6a6
stanley akyea <stanley.akyea@gmail.com>
2019-03-11 21:57:37 +0000

    add debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
   
-  #set email to downcase at the database layer
-  before_save {self.email = email.downcase}
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
   validates :user_id, presence: true
