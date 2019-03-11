4ec23a0d02636ae0470362e656000e1fd208a2bd
stanley akyea <stanley.akyea@gmail.com>
2019-03-11 20:58:14 +0000

    add user association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true
+  belongs_to :user
+  
+  #set email to downcase at the database layer
+  before_save {self.email = email.downcase}
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
+  validates :user_id, presence: true
 end
