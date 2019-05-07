0cf5a70b2dfce035fd69accf6c8ad306638193fb
stanley akyea <stanley.akyea@gmail.com>
2019-05-07 08:34:03 +0000

    Complete authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+  
+  protect_from_forgery with: :exception
+  
+  helper_method :current_user, :logged_in?
+  
+  
+  
+  def current_user
+    #return current user if current user already exists, if not find user by id
+   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  def logged_in?
+    #check if current user is logged in using boolean converter !!
+    !!current_user
+  end
+  
+  def require_user
+    #restrict certain actions based on if there is a user or not
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
+    end
+    
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
   def index
     @users = User.paginate(page: params[:page], per_page: 2)
   end
@@ -10,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:sucess] = "Welcome to the Alpha Blog #{@user.username}"
+      flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
@@ -19,11 +21,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+   
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated sucessfully"
       redirect_to articles_path
@@ -33,13 +34,23 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
     @user_article = @user.articles.paginate(page: params[:page], per_page: 2)
   end
   
   private 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can not edit another user's account"
+      redirect_to root_path
+    end
   end
   
 end
