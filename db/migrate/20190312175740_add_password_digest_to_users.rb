d889fd1ffe88c273ac262b4c413d9b413298ce94
stanley akyea <stanley.akyea@gmail.com>
2019-05-10 22:25:37 +0000

    Add Admin user functionalities

--- db/migrate/20190312175740_add_password_digest_to_users.rb
+++ db/migrate/20190312175740_add_password_digest_to_users.rb
@@ -1,5 +1,6 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[5.2]
   def change
+              #table name #column title #data type
     add_column :users, :password_digest, :string
   end
 end
